62bbd5323cb73ba9bc7d0788da9d378a9100a79c
elvesbd <elvesbd@yahoo.com>
2021-12-28 14:26:46 -0300

    feat: create posts controllers and posts dto

---
+++ src/posts/controllers/posts.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreatePostDto, UpdatePostDto } from '../infra/dto';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(':id')
+  getPostById(@Param('id') id: string) {
+    return this.postsService.getAllPosts();
+  }
+
+  @Post()
+  async createPost(@Body() post: CreatePostDto) {
+    return await this.postsService.createPost(post);
+  }
+
+  @Put(':id')
+  async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return await this.postsService.replacePost(+id, post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: string) {
+    return await this.postsService.deletePost(+id);
+  }
+}
