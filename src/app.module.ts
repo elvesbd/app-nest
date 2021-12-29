ab37d0f96e9ec64db69816488f7c3fd321794e47
elvesbd <elvesbd@yahoo.com>
2021-12-29 13:09:03 -0300

    fix: implemented config schema database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
-import * as Joi from 'joi';
+import { configValidationSchema } from './database';
 
 @Module({
   imports: [
-    PostsModule,
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
-      }),
+      envFilePath: [`.env.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
+    PostsModule,
     DatabaseModule,
   ],
   controllers: [],
