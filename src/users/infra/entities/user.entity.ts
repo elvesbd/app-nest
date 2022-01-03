710562e17593944931e9283b052ca78effd6031f
elvesbd <elvesbd@yahoo.com>
2022-01-03 16:45:54 -0300

    fix: addend Exclude and Expose

--- src/users/infra/entities/user.entity.ts
+++ src/users/infra/entities/user.entity.ts
@@ -1,20 +1,25 @@
 import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { hashSync } from 'bcrypt';
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Expose()
   @Column({ unique: true })
   email: string;
 
+  @Expose()
   @Column()
   name: string;
 
+  @Exclude()
   @Column()
   password: string;
 
+  @Expose()
   @Column()
   phoneNumber: string;
 
